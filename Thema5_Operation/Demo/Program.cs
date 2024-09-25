c05859cc10b4fb1f6da6baee6d4bcece93987245
Halil <halilski1979@gmail.com>
2024-09-25 10:19:26 +0300

    awee

---
+++ Thema5_Operation/Demo/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Name: ");
+            string name=Console.ReadLine();
+            Console.Write("Klas: ");
+
+            int klas=int.Parse(Console.ReadLine());
+            Console.Write("Paralelka: ");
+
+            char paralelka=char.Parse(Console.ReadLine());
+            Console.Write("Uspeh: ");
+
+            double uspeh=double.Parse(Console.ReadLine());
+            bool stipendia = (uspeh>=5.50);
+
+
+            Console.WriteLine("--------------");
+            Console.WriteLine($"I am {name}, {klas} {paralelka} class. My grade is {uspeh}. Stipeendia: {stipendia}");
+
+        }
+    }
+}
